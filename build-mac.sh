8d6270a92cbf6b7d50004b82c24a25635705e627
Ryo Shen <shenlw66@gmail.com>
2024-12-10 16:49:11 +0800

    update

--- build-mac.sh
+++ build-mac.sh
@@ -22,9 +22,9 @@ fi
 echo "Starting building postgres binaries"
 
 wget -O patchelf.tar.gz "https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.gz"
-mkdir -p /usr/src/patchelf
-tar -xf patchelf.tar.gz -C /usr/src/patchelf --strip-components 1
-cd /usr/src/patchelf
+mkdir -p /Users/runner/local/patchelf
+tar -xf patchelf.tar.gz -C /Users/runner/local/patchelf --strip-components 1
+cd /Users/runner/local/patchelf
 wget -O config.guess "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=b8ee5f79949d1d40e8820a774d813660e1be52d3"
 wget -O config.sub "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=b8ee5f79949d1d40e8820a774d813660e1be52d3"
 ./configure --prefix=/usr/local
