4550b63d1d637086f767f104a6730dcbe5604f09
Ryo Shen <shenlw66@gmail.com>
2024-12-10 16:46:47 +0800

    update

--- build-mac.sh
+++ build-mac.sh
@@ -21,6 +21,16 @@ fi
 
 echo "Starting building postgres binaries"
 
+wget -O patchelf.tar.gz "https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.gz"
+mkdir -p /usr/src/patchelf
+tar -xf patchelf.tar.gz -C /usr/src/patchelf --strip-components 1
+cd /usr/src/patchelf
+wget -O config.guess "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=b8ee5f79949d1d40e8820a774d813660e1be52d3"
+wget -O config.sub "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=b8ee5f79949d1d40e8820a774d813660e1be52d3"
+./configure --prefix=/usr/local
+make -j$(sysctl -n hw.physicalcpu)
+make install
+
 wget -O postgresql.tar.bz2 "https://ftp.postgresql.org/pub/source/v$PG_VERSION/postgresql-$PG_VERSION.tar.bz2"
 mkdir -p /Users/runner/local/postgresql
 tar -xf postgresql.tar.bz2 -C /Users/runner/local/postgresql --strip-components 1
@@ -30,8 +40,9 @@ cd /Users/runner/local/postgresql
     --prefix=/Users/runner/build/pg-build \
     --without-icu \
     --without-readline
-make -j
-make install
+make -j$(sysctl -n hw.physicalcpu) world-bin
+make install-world-bin
+make -C contrib install
 
 mkdir -p /Users/runner/local/pgvector
 curl -sL "https://github.com/pgvector/pgvector/archive/refs/tags/v$PGVECTOR_VERSION.tar.gz" | tar -xzf - -C /Users/runner/local/pgvector --strip-components 1
